26aa977a47828de5db16e2db6aae1984900c3d47
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-06 09:18:39 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for num in range (0, 100):
+for num in range (99):
     print("{:02d}".format(num), end=", ")
 else:
-    print("{:02d}".format(num))
+    print("{:02d}".format(num + 1))
