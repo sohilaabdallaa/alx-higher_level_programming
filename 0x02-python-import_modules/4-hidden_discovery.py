aed8fdfe84298a7c8d11eb05df125f563600b730
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-08 13:57:42 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
+    import hidden_4
     for name in dir(hidden_4):
         if name[:2] != "__":
             print("{}".format(name[i]))
