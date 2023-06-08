ab7af1b4f23bb851df935a93186edf4a4cce397e
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-08 13:06:18 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if l == 1:
         print("{:d} arguments.".format(0))
     elif l == 2:
-        print("{:d} arguments:".format(1))
+        print("{:d} argument:".format(1))
     else:
-        print("{:d} arguments".format(l))
+        print("{:d} arguments".format(l - 1))
 
     for i in range(1, l):
         print("{:d}: {}".format(i, sys.argv[i]))
