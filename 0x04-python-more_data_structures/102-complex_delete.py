c4c6d9edc0f4f9946a89ea8733b81b5755a1fa7f
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-16 12:59:58 -0700

    complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary is None:
+        return None
+    for key in list(a_dictionary):
+        if (a_dictionary[key] == value):
+            del a_dictionary[key]
+    return a_dictionary
