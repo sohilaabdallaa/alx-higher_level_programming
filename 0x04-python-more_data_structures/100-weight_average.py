5aa792362480b92adcd0d81aacee051abbe35535
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-16 13:12:48 -0700

    weigth avg

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,9 @@
 def weight_average(my_list=[]):
     division = 0
     av_weigth = 0
+    if my_list is None:
+        return 0
     for i in my_list:
         av_weigth += i[0] * i[1]
         division += i[1]
-    return float(av_weigth / division) 
+    return float(av_weigth/division) 
