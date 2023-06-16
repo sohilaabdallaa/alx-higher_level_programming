fffb5378c4f5970ff71cd5a2d4f98fe737c81909
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-16 11:30:41 -0700

    maximum

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,11 @@
 def best_score(a_dictionary):
     if a_dictionary is None or not isinstance(a_dictionary, dict):
         return None
-    return max(a_dictionary, key = a_dictionary.get)
+    if len(a_dictionary) == 0:
+        return None
+    maxi = -1
+    for key, value in a_dictionary.items():
+        if maxi < a_dictionary[key]:
+            maxi = a_dictionary[key]
+            maxkey = key
+    return maxkey
