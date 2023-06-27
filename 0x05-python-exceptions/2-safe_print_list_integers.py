2380661d4f2348b90d4d8b39550b9b3bd863d166
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-26 17:06:58 -0700

    task 3

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    total = 0
+    for c in range(x):
+        try:
+            print("{:d}".format(my_list[c]), end="")
+            total += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return total
