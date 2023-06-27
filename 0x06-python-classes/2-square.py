431aac7f838df293d20b56eca277457d08a20ad3
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-27 15:50:49 -0700

    2-task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,15 +10,14 @@ class Square:
     to initialize size
     """
     def __init__(self, size=0):
-        """ initialize __size private instance variable """
-        try:
+        """ if statment """
+        if (size < 0):
+            """ raise an error """
+            raise ValueError("size must be >= 0")
+        elif type(size) != int:
+            """ raise an error """
+            raise TypeError("size must be an integer")
+        else:
             """ initialize __size private instance variable
             """
             self. __size = size
-            """ if statment """
-            if (size < 0):
-                """ raise an error """
-                raise ValueError("size must be >= 0")
-            elif type(size) != int:
-                """ raise an error """
-                raise TypeError("size must be an integer")
