55b0b1f334cc58dfaea4650c5ff1e7f230cd74fc
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-27 15:35:39 -0700

    1-task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+No Modules imported
+"""
+
+
+class Square:
+    """
+    define a function called __init__
+    to initialize size
+    """
+    def __init__(self, size):
+        """initialize __size private instance variable
+        """
+        self. __size = size
