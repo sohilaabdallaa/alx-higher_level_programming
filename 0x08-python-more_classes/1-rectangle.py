b5ae72b344ef477f8d7a8665b9d974c31a7669fa
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-03 19:02:53 -0700

    setter & getter

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" No Modules Imported """
+
+
+class Rectangle:
+    """ Rectangle Class
+        private instance attribute width
+        private instance attribute heigth
+    """
+    def __init__(self, width=0, height=0):
+        """ Instantiation of width & heigth """
+        self.width = width
+        self.heigth = heigth
+
+    @property
+    def width(self):
+        """ width getter to return width attribute """
+        return self.__width
+
+    @property
+    def heigth(self):
+        """ heigth getter to return heigth value """
+        return self.__heigth
+
+    def width(self, value):
+        """ width setter to set value
+            for private attribute width
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def heigth(self, value):
+        """ heigth setter """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
