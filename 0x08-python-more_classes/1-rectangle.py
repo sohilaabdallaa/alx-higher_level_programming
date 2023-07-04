4ab189d47bbdb4290e2c7650ff5394ecc8d17c67
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-03 19:28:39 -0700

    setter & getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,13 +6,13 @@ class Rectangle:
     """ Rectangle Class """
     def __init__(self, width=0, height=0):
         """ Instantiation of width & heigth """
-       if not isinstance(width, int):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        if type(value) is not int:
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
         self.__width = width
         self__.height = height
@@ -27,9 +27,9 @@ class Rectangle:
         """ width setter to set value
             for private attribute width
         """
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
