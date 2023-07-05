30a66c0c600db2881ac5f74446c186d7ac15f727
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-04 19:20:56 -0700

    test indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,11 +8,20 @@ def text_indentation(text):
     prints a text with 2 new lines \
             after each of these characters: ., ? and :
     """
+    is_special = 0
     if type(text) is not str:
         raise TypeError("text must be a string")
     for c in text:
-        if c == '?' or c == '.' or c == ':':
-            print(c)
-            print()
-        else:
-            print(c, end="")
+        if is_special == 0:
+            if c == '':
+                continue
+            else:
+                is_special = 1
+
+        if is_special == 1:
+            if c == '?' or c == '.' or c == ':':
+                print(c)
+                print()
+                is_special = 0
+            else:
+                print(c, end="")
