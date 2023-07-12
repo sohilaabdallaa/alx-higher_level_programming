90d4a7c4564cdc05a4eb54bd99eed2596d316ed5
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-11 19:50:20 -0700

    student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    No Module imported
+"""
+
+
+class Student:
+    """Defines student class"""
+    def __init__(self, first_name, last_name, age):
+        """
+            Instantiation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+            retrieves a dictionary representation of Student instance
+        """
+        if type(attrs) == list and all(type(elem) == str for elem in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+        def reload_from_json(self, json):
+        """ Loads from json."""
+        for key, value in json.items():
+            self.__dict__[key] = value
