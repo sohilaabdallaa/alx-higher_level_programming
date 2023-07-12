a2dbdb551818c683aed3f0ebe8482d94934bc9ea
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-11 19:54:30 -0700

    pascal_triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+No Module imported
+Module for pascal_triangle method.
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a  list of lists of integers
+    """
+    rows = [[1 for j in range(i + 1)] for i in range(n)]
+    for n in range(n):
+        for i in range(n - 1):
+            rows[n][i + 1] = sum(rows[n - 1][i:i + 2])
+    return rows
