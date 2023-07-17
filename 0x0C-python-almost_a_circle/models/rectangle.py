7478474a7c920ab2a860abe06b5fc52e2fa79f65
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-17 02:26:11 -0700

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,3 +107,7 @@ class Rectangle(Base):
             for y in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """represents the class objects as a string"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
